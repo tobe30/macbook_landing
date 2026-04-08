3494a262de2623d2838db701f74a90452b2835b8
Tobe Marizu <tobemarizu@gmail.com>
2026-04-08 13:44:36 +0100

    Hero section1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,12 @@ const Hero = () => {
             <img src="/title.png" alt="Macbook title" />
         </div>
 
-        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline></video>
-
+        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+        
         <button>Buy</button>
 
         <p>From $1599 or $133/mo for 12 months</p>
+        
     </section>
   )
 }
