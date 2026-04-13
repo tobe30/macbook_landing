e3d5ccbcf4cc877349c378984c250f13674095d9
Tobe Marizu <tobemarizu@gmail.com>
2026-04-13 15:36:05 +0100

    showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all'
+import Showcase from './components/Showcase';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
    <main>
     <Navbar/>
     <Hero/>
+    <ProductViewer/>
+    <Showcase/>
    </main>
   )
 }
