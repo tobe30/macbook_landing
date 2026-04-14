6976fa8e2fa94f184dd7c8c8e49860ebd62be0a9
Tobe Marizu <tobemarizu@gmail.com>
2026-04-14 14:57:49 +0100

    implement P

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import ProductViewer from './components/ProductViewer'
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all'
 import Showcase from './components/Showcase';
+import Performance from './components/Performance';
+import Highlight from './components/Highlight';
+import Footer from './components/Footer';
+import Features from './components/Features';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,6 +19,10 @@ const App = () => {
     <Hero/>
     <ProductViewer/>
     <Showcase/>
+    <Performance/>
+    <Features/>
+    <Highlight/>
+    <Footer/>
    </main>
   )
 }
